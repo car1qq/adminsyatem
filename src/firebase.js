7dc00a88b3b7666a1dba2d45ada367e8de0e574e
car1qq <94748125+car1qq@users.noreply.github.com>
2022-07-14 15:52:01 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,14 @@ import 'firebase/storage';
 import 'firebase/functions';
 
 const config = {
-  apiKey: process.env.REACT_APP_FIRE_BASE_KEY,
-  authDomain: process.env.REACT_APP_FIRE_BASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIRE_BASE_DB_URL,
-  projectId: process.env.REACT_APP_FIRE_BASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIRE_BASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIRE_BASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIRE_BASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIRE_BASE_MEASURMENT_ID,
+apiKey: "AIzaSyBsPuDiX8qJs1pmtj7fDPqpEwFeo4SCdvQ",
+  authDomain: "socialapp-17226.firebaseapp.com",
+  databaseURL: "https://socialapp-17226-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "socialapp-17226",
+  storageBucket: "socialapp-17226.appspot.com",
+  messagingSenderId: "605228920802",
+  appId: "1:605228920802:web:1f511d41f45335e11f93bb",
+  measurementId: "G-W4XDY7XRP5"
 };
 
 firebase.initializeApp(config);
